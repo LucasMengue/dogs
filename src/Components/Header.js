676f4deb1d7251507be603a6cf15bae60931e4cc
lucas <lucasmengue96@hotmail.com>
2022-10-06 12:26:29 -0300

    added a button to logout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  const { data } = React.useContext(UserContext);
+  const { data, userLogout } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -19,6 +19,7 @@ const Header = () => {
         {data ? (
           <Link className={styles.login} to="/conta">
             {data.nome}
+            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
