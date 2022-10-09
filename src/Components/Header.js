d04556a9da1db442bf58d411c3bb5a55896e99bf
lucas <lucasmengue96@hotmail.com>
2022-10-09 20:17:14 -0300

    deleted sair button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  const { data, userLogout } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -19,7 +19,6 @@ const Header = () => {
         {data ? (
           <Link className={styles.login} to="/conta">
             {data.nome}
-            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
