013f28efc7346323ce3b989a8b1996d7bcf98ec6
lucas <lucasmengue96@hotmail.com>
2022-10-09 20:40:50 -0300

    fixed NavLink active

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -16,15 +16,25 @@ const UserHeaderNav = () => {
 
   return (
     <nav className={styles.nav}>
-      <NavLink to="/conta" end activeClassName={styles.active}>
+      <NavLink
+        to="/conta"
+        end
+        className={(navData) => (navData.isActive ? styles.active : "")}
+      >
         <MinhasFotos />
         {mobile && "Minhas Fotos"}
       </NavLink>
-      <NavLink to="/conta/estatisticas" activeClassName={styles.active}>
+      <NavLink
+        to="/conta/estatisticas"
+        className={(navData) => (navData.isActive ? styles.active : "")}
+      >
         <Estatisticas />
         {mobile && "Estatísticas"}
       </NavLink>
-      <NavLink to="/conta/postar" activeClassName={styles.active}>
+      <NavLink
+        to="/conta/postar"
+        className={(navData) => (navData.isActive ? styles.active : "")}
+      >
         <AdicionarFoto />
         {mobile && "Adicionar Foto"}
       </NavLink>
